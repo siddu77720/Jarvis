254f3a1fbc4fa473985d467f47e452e229b28292
siddu77720 <siddushinde80@gmail.com>
2025-06-01 21:47:33 +0530

    hood design done

---
+++ www/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    
+    $('.text').texllate({
+        loop:true,
+        sync:true,
+        in:{
+            effect:"bounceIn",
+            
+        },
+        out:{
+            effect:"bounceOut",
+
+        },
+
+    });
+});
